293b2ff27d9bcee16f2b9ddc4f6843d177471db0
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-02-09 12:43:13 +0100

    added home.html

--- website/views.py
+++ website/views.py
@@ -1,9 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return "<h1>This is the home page</h1>"
+    return render_template("home.html")
 
