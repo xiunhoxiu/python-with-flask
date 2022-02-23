e9d72d326465325f41a3b8d71bc93b29f341e5b4
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-02-23 10:36:29 +0100

    Update __init__.py: Flask SQLAlchemy Setup

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+
+db = SQLAlchemy()  # object to add/create new user to the db
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'asaldlfjlk alfakjie aklsjfl'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'  # sqlite or SQL db is located/stored at f'sqlite:///{DB_NAME}' using flask within the website folder
+    db.init_app(app)  # using the Flask app with the db.
 
     from .views import views
     from .auth import auth
