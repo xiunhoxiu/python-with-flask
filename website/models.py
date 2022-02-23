82e59de48cedc7dbd95c810dbc8dc0beee3b27db
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-02-23 11:28:13 +0100

    update models.py
    Added database Models

---
+++ website/models.py
@@ -0,0 +1,25 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now)  
+        # func: gets the current date and time and store in the DateTime field
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))  
+        # associate different info with different users via a foreign key
+        # foreign key (column): always ref an ID of another db.Column
+    
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)  
+        # uniquely identify an object - primary key = unique identifier (typically an integer)
+
+    email = db.Column(db.String(150), unique=True) 
+        # max length of character, email is unique
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    notes = db.relationship('Note')
