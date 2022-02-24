e6b34d817fe1b7cff67b275af0588b76a9f60bbe
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-02-24 10:12:44 +0100

    Created new user accounts

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,10 @@
 # from xmlrpc.client import boolean
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash  
+    # hash func. that converts the actual password to something else
+    # one-way function that does not have an inverse
+from . import db
 
 
 auth = Blueprint('auth', __name__)
@@ -20,19 +25,24 @@ def logout():
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
+        first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(email) < 4:
-            flash('Email must be greater than 3 characters.', category='error')
-        elif len(firstName) < 2:
+        if len(email) < 3:
+            flash('Email must be greater than 2 characters.', category='error')
+        elif len(first_name) < 2:
             flash('First name must be greater than 1 character.', category='error')
         elif password1 != password2:
             flash('Passwords don\'t match.', category='error')
-        elif len(password1) < 7:
+        elif len(password1) < 8:
             flash('Password must be at least 7 characters.', category='error')
         else:
-            flash('Account created!', category='sucess')
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Account created!', category='success')
+            return redirect(url_for('views.home'))
+
 
     return render_template("sign_up.html")
